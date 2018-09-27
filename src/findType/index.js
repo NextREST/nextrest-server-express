46d342615af1882076766eb799a0fb44ab58809d
DragonRaider5 <marces.engel@googlemail.com>
2018-09-27 19:15:49 +0200

    Implemented findType

--- src/findType/index.js
+++ src/findType/index.js
@@ -1,1 +1,49 @@
-module.exports.findType = () => {}
+const isEven = function (num) {
+  return num % 2 === 0
+}
+
+const findType = function findTypeByPath (types, url) {
+  const path = url.split('/').slice(1)
+
+  let type = types
+  let referrer = null
+  for (let i = 0; i < path.length; i++) {
+    const section = path[i]
+
+    if (isEven(i)) {
+      type = (
+        i === 0
+          ? type[section]
+          : type.outboundFields[section]
+      ) || false
+
+      if (!type) {
+        break
+      }
+
+      if (i === 0) {
+        continue
+      }
+
+      referrer = {
+        id: path[i - 1],
+        type: path[i - 2],
+        referrer
+      }
+
+      continue
+    }
+  }
+
+  return {
+    id: (isEven(path.length) && type)
+      ? path[path.length - 1]
+      : null,
+    type,
+    referrer
+  }
+}
+
+module.exports = {
+  findType
+}
---
+++ src/findType/index.test.js
@@ -0,0 +1,93 @@
+const casual = require('casual')
+const { findType } = require('./index.js')
+
+describe('findType()', () => {
+  it('can find root types', () => {
+    const resourceName = casual.word
+    const types = {
+      [resourceName]: {
+        get: () => null
+      }
+    }
+
+    const { type, id, referrer } = findType(types, `/${resourceName}`)
+
+    expect(type).toBe(types[resourceName])
+    expect(id).toEqual(null)
+    expect(referrer).toEqual(null)
+  })
+
+  it('returns type: false if it found no matching type', () => {
+    const { type, id, referrer } = findType({}, `/${casual.word}/${casual.uuid}`)
+
+    expect(type).toBe(false)
+    expect(id).toEqual(null)
+    expect(referrer).toEqual(null)
+  })
+
+  it('returns the id if a specific entity was requested', () => {
+    const resourceName = casual.word
+    const types = {
+      [resourceName]: {
+        get: () => null
+      }
+    }
+
+    const entityId = casual.uuid
+    const { type, id, referrer } = findType(types, `/${resourceName}/${entityId}`)
+
+    expect(type).toBe(types[resourceName])
+    expect(id).toBe(entityId)
+    expect(referrer).toEqual(null)
+  })
+
+  it('properly handles nested routes', () => {
+    const resourceNames = [
+      casual.word,
+      casual.word,
+      casual.word
+    ]
+
+    const types = {
+      [resourceNames[0]]: {
+        outboundFields: {
+          [resourceNames[1]]: {
+            outboundFields: {
+              [resourceNames[2]]: {
+                get: () => null
+              }
+            }
+          }
+        }
+      }
+    }
+
+    const ids = [
+      casual.uuid,
+      casual.uuid,
+      casual.uuid
+    ]
+    const { type, id, referrer } = findType(
+      types,
+      `/${resourceNames[0]}/${ids[0]}/${resourceNames[1]}/${ids[1]}/${resourceNames[2]}/${ids[2]}`
+    )
+
+    expect(type).toBe(
+      types[resourceNames[0]]
+        .outboundFields[resourceNames[1]]
+        .outboundFields[resourceNames[2]]
+    )
+
+    expect(id).toBe(ids[2])
+
+    expect(referrer).toEqual({
+      id: ids[1],
+      type: resourceNames[1],
+      referrer: {
+        id: ids[0],
+        type: resourceNames[0],
+        referrer: null
+      }
+    })
+  })
+})
